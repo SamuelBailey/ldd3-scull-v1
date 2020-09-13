0c31f906de779fa9d7272f835196a4b3fa5f64f7
Samuel Bailey <samuelrbailey19@gmail.com>
2020-09-13 21:12:57 +0100

    Requests a major version, creates a device called 'jeff' and then removes it

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ ifneq ($(KERNELRELEASE),)
 # Otherwise we were called directly from the command
 # line; invoke the kernel build system.
 else
-    KERNELDIR ?= /lib/modules/5.4.0-45-generic/build
+    KERNELDIR ?= /lib/modules/$(shell uname -r)/build
     PWD  := $(shell pwd)
 
 default:
--- main.c
+++ main.c
@@ -5,16 +5,33 @@
 // For registering devices
 #include <linux/fs.h>
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
+
+dev_t dev;
+unsigned int count;
 
 static int __init create_devices(void) {
+    int firstminor = 0;
+    char* name = "jeff";
+    count = 1;
+
+    printk(KERN_ALERT "Printing kernel alert\n");
+
+    alloc_chrdev_region(&dev, firstminor, count, name);
+
+    printk(KERN_ALERT "Created device\n");
+    printk(KERN_ALERT "Major version: %u\n", MAJOR(dev));
+    printk(KERN_ALERT "Minor version: %u\n", MINOR(dev));
+
     printk(KERN_DEBUG "created devices\n");
     return 0;
 }
 
 static void __exit destroy_devices(void) {
+    unregister_chrdev_region(dev, count);
+
     printk(KERN_DEBUG "destroyed devices\n");
 }
 
 module_init(create_devices);
-module_exit(destroy_devices)
+module_exit(destroy_devices);
